f74adf40300285839a058ddcc72b2660f25a1697
Richard <richard.upwork.awj5R@protonmail.com>
2023-07-07 01:26:12 -0400

    Update market_maker_tb.sv

--- market_maker_tb.sv
+++ market_maker_tb.sv
@@ -1,48 +1,137 @@
-`timescale 1ns / 1ps
+// Test scenarios for functional coverage
 
-module market_maker_tb;
+// Reservation Price Coverage
+initial begin
+  // Test different reservation price values
+  reservation_price = 8'h80;    // Example reservation price
+  #10;
+  reservation_price = 8'hC0;
+  #10;
+  reservation_price = 8'h40;
+  #10;
+  // Test reservation prices close to the current market price
+  reservation_price = current_price - 8'h08;    // Reservation price slightly below current price
+  #10;
+  reservation_price = current_price + 8'h08;    // Reservation price slightly above current price
+  #10;
+end
 
-  // Inputs
-  reg clk;
-  reg reset;
-  // Add other input signals as required for your strategy
+// Trend Detection Coverage
+initial begin
+  // Test increasing price trend
+  for (int i = 0; i < 10; i = i + 1) begin
+    current_price = previous_price + i;
+    #10;
+  end
+
+  // Test decreasing price trend
+  for (int i = 10; i > 0; i = i - 1) begin
+    current_price = previous_price - i;
+    #10;
+  end
+
+  // Test stable price
+  current_price = previous_price;
+  #10;
+end
+
+// Risk Monitoring Coverage
+initial begin
+  // Test market prices crossing risk threshold above reservation price
+  current_price = reservation_price + risk_threshold + 8'h02;    // Above risk threshold
+  #10;
+  current_price = reservation_price + risk_threshold - 8'h02;    // Below risk threshold
+  #10;
+
+  // Test market prices crossing risk threshold below reservation price
+  current_price = reservation_price - risk_threshold - 8'h02;    // Below risk threshold
+  #10;
+  current_price = reservation_price - risk_threshold + 8'h02;    // Above risk threshold
+  #10;
+end
+
+// Bid-Ask Spread Adjustment Coverage
+initial begin
+  // Test condition for adjusting bid-ask spread
+  // Adjusting bid-ask spread when condition is met
   // ...
 
-  // Outputs
-  wire buy_order;
-  wire sell_order;
+  // Test condition for maintaining reservation price without adjusting spread
+  // ...
 
-  // Instantiate the market_maker module
-  market_maker dut (
-    .clk(clk),
-    .reset(reset),
-    // Connect other input signals
-    // ...
-    .buy_order(buy_order),
-    .sell_order(sell_order)
-  );
+  // Test bid-ask spread adjustment when condition is not met
+  // ...
 
-  // Clock generation
-  always #5 clk = ~clk;
+  // Test bid-ask spread adjustment at different reservation price levels
+  // ...
+end
 
-  // Reset initialization
-  initial begin
-    reset = 1'b1;
-    #10 reset = 1'b0;
-  end
+// Input Range Coverage
+initial begin
+  // Test full range of quantity values
+  q = 8'h00;          // Minimum value
+  #10;
+  q = 8'hFF;          // Maximum value
+  #10;
+  q = 8'h7F;          // Middle value
+  #10;
+  q = -8'h40;         // Negative value
+  #10;
 
-  // Monitor
-  always @(posedge clk) begin
-    $display("Buy Order: %b, Sell Order: %b", buy_order, sell_order);
-    // Add additional monitoring statements as needed
-    // ...
-  end
+  // Test full range of current time values
+  t = 8'h00;          // Minimum value
+  #10;
+  t = 8'hFF;          // Maximum value
+  #10;
+  t = 8'h7F;          // Middle value
+  #10;
 
-  // Stimulus
-  initial begin
-    // Add stimulus code to drive inputs
-    // ...
-    #100 $finish; // Terminate simulation after a certain time
-  end
+  // Test full range of bid spread values
+  delta_a = 8'h00;    // Minimum value
+  #10;
+  delta_a = 8'hFF;    // Maximum value
+  #10;
+  delta_a = 8'h7F;    // Middle value
+  #10;
 
-endmodule
+  // Test full range of ask spread values
+  delta_b = 8'h00;    // Minimum value
+  #10;
+  delta_b = 8'hFF;    // Maximum value
+  #10;
+  delta_b = 8'h7F;    // Middle value
+  #10;
+end
+
+// Combination Coverage
+initial begin
+  // Test combination of different scenarios
+  // ...
+
+  // Test various reservation prices, trends, risk threshold crossings, and input ranges together
+  // ...
+end
+
+// Error and Exception Coverage
+initial begin
+  // Test boundary conditions, edge cases, and invalid inputs
+  // ...
+
+  // Test error scenarios such as overflow, underflow, and division by zero
+  // ...
+end
+
+// Stress Testing Coverage
+initial begin
+  // Test scenarios with high transaction rates and rapid market price fluctuations
+  // ...
+
+  // Test extreme input values to evaluate stability and performance
+  // ...
+end
+
+// Monitor for functional coverage analysis
+always @(posedge clk) begin
+  if (!reset)
+    $coverage;    // Display functional coverage metrics
+end
