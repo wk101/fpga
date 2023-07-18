9d5ed31a6608ef63e8c0897f77058a5e46c918f5
Richard <richard.upwork.awj5R@protonmail.com>
2023-07-18 15:00:21 -0400

    Update market_maker_sim.cpp

--- market_maker_sim.cpp
+++ market_maker_sim.cpp
@@ -5,7 +5,6 @@
 #include <thread>
 
 struct MarketData {
-    int symbolId; // Use integer IDs to identify symbols
     double midPrice; // Use mid price for trading
 };
 
@@ -18,15 +17,15 @@ double generatePrice(double previousPrice) {
 }
 
 // Function to buy a quantity of a symbol
-void buy(std::vector<double>& positions, int symbolId, double price, int quantity) {
-    positions[symbolId - 1] += quantity;
-    std::cout << "Buy Order - Symbol ID: " << symbolId << " | Quantity: " << quantity << " | Price: " << price << "\n";
+void buy(std::vector<double>& positions, double price, int quantity) {
+    positions[0] += quantity; // Symbol ID 1 corresponds to index 0
+    std::cout << "Buy Order - Symbol ID: 1 | Quantity: " << quantity << " | Price: " << price << "\n";
 }
 
 // Function to sell a quantity of a symbol
-void sell(std::vector<double>& positions, int symbolId, double price, int quantity) {
-    positions[symbolId - 1] -= quantity;
-    std::cout << "Sell Order - Symbol ID: " << symbolId << " | Quantity: " << quantity << " | Price: " << price << "\n";
+void sell(std::vector<double>& positions, double price, int quantity) {
+    positions[1] -= quantity; // Symbol ID 2 corresponds to index 1
+    std::cout << "Sell Order - Symbol ID: 2 | Quantity: " << quantity << " | Price: " << price << "\n";
 }
 
 // Function to calculate PNL for a position based on the current mid price
@@ -35,53 +34,66 @@ double calculatePNL(double positionQuantity, double midPrice) {
 }
 
 int main() {
-    const std::vector<int> cryptoSymbolIds = {1, 2}; // Use integer IDs to identify symbols (1 and 2)
     const double initialPrice = 100.0;
 
-    std::vector<MarketData> marketData; // Data for multiple crypto symbols
+    std::vector<MarketData> marketData(2); // Data for 2 crypto symbols
 
     // Initialize market data for each crypto symbol
-    for (int i = 0; i < cryptoSymbolIds.size(); ++i) {
-        MarketData data;
-        data.symbolId = cryptoSymbolIds[i];
-        data.midPrice = initialPrice;
-        marketData.push_back(data);
-    }
+    marketData[0].midPrice = initialPrice; // Symbol ID 1 corresponds to index 0
+    marketData[1].midPrice = initialPrice; // Symbol ID 2 corresponds to index 1
 
-    std::vector<double> positions(cryptoSymbolIds.size(), 0); // Vector to keep track of positions
+    std::vector<double> positionsSymbol1(1, 0.0); // Vector to keep track of positions for symbol 1
+    std::vector<double> positionsSymbol2(1, 0.0); // Vector to keep track of positions for symbol 2
 
     // Generate fake market data every half second for 30 seconds (reduce for faster testing)
     for (int i = 0; i < 60; ++i) {
         std::cout << "Updating Market Data - Second " << (i + 1) << "\n";
-        for (int j = 0; j < marketData.size(); ++j) {
-            MarketData& data = marketData[j];
+        for (int symbolId = 1; symbolId <= 2; ++symbolId) {
+            MarketData& data = marketData[symbolId - 1]; // Adjust the index to access the correct symbol's data
             data.midPrice = generatePrice(data.midPrice);
 
             // Buy and sell randomly
-            int symbolId = data.symbolId;
             double price = data.midPrice;
             int quantity = 100; // For simplicity, assume fixed quantity
 
             // Randomly decide to buy or sell
             if (std::rand() % 2 == 0) {
-                buy(positions, symbolId, price, quantity);
+                if (symbolId == 1) {
+                    buy(positionsSymbol1, price, quantity);
+                } else if (symbolId == 2) {
+                    buy(positionsSymbol2, price, quantity);
+                }
             } else {
-                sell(positions, symbolId, price, quantity);
+                if (symbolId == 1) {
+                    sell(positionsSymbol1, price, quantity);
+                } else if (symbolId == 2) {
+                    sell(positionsSymbol2, price, quantity);
+                }
             }
         }
 
         if ((i + 1) % 10 == 0) {
-            // Close positions and calculate PNL every 5 seconds
+            // Close positions and calculate PNL every 10 seconds
             std::cout << "Closing Positions - Second " << (i + 1) << "\n";
             double totalPNL = 0.0;
-            for (int j = 0; j < positions.size(); ++j) {
-                double pnl = calculatePNL(positions[j], marketData[j].midPrice);
-                totalPNL += pnl;
-                std::cout << "Position Symbol ID: " << (j + 1) << " | Quantity: " << positions[j] << " | PNL: " << pnl << "\n";
-            }
+
+            // Calculate PNL for each symbol and reset positions
+            double pnlSymbol1 = calculatePNL(positionsSymbol1[0], marketData[0].midPrice);
+            double pnlSymbol2 = calculatePNL(positionsSymbol2[0], marketData[1].midPrice);
+
+            // Add the positions for both symbols together to get the overall position
+            double totalPosition = positionsSymbol1[0] + positionsSymbol2[0];
+
+            totalPNL = pnlSymbol1 + pnlSymbol2;
+            std::cout << "Position Symbol ID: 1 | Quantity: " << positionsSymbol1[0] << " | PNL: " << pnlSymbol1 << "\n";
+            std::cout << "Position Symbol ID: 2 | Quantity: " << positionsSymbol2[0] << " | PNL: " << pnlSymbol2 << "\n";
+            std::cout << "Total Position: " << totalPosition << "\n";
             std::cout << "Total PNL: " << totalPNL << "\n";
             std::cout << "-----------------------------\n";
-            positions.assign(cryptoSymbolIds.size(), 0); // Close all positions
+
+            // Reset positions for both symbols
+            positionsSymbol1[0] = 0.0;
+            positionsSymbol2[0] = 0.0;
         }
 
         // Simulate half-second interval
